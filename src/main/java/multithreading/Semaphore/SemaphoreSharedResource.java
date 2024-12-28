7ca56dacea1f8b6990821e7de9fcc5d942c6f2a3
RAY <vibhuraj23@gmail.com>
2024-12-28 21:46:00 +0530

    semaphore lock

---
+++ src/main/java/multithreading/Semaphore/SemaphoreSharedResource.java
@@ -0,0 +1,25 @@
+package main.java.multithreading.Semaphore;
+
+import java.util.concurrent.Semaphore;
+
+public class SemaphoreSharedResource {
+
+    boolean isAvailable = false;
+
+    Semaphore semaphore = new Semaphore(2);
+    // permits : number of threads that can acquire thread at a time
+
+    public void producer(){
+        try{
+            semaphore.acquire();
+            System.out.println("Producer Lock acquired by : " + Thread.currentThread().getName());
+            isAvailable = true;
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            System.out.println("Producer Lock released by : " + Thread.currentThread().getName());
+            semaphore.release();
+        }
+    }
+}
